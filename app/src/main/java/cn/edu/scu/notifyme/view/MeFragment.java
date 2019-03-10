98d6176b6e07bb66fffffd9841fb401ad8d80726
tinyAdapter <tinyadapter@outlook.com>
2019-03-10 10:21:15 +0800

    创建设置页面 + 跳转

--- app/src/main/java/cn/edu/scu/notifyme/view/MeFragment.java
+++ app/src/main/java/cn/edu/scu/notifyme/view/MeFragment.java
@@ -1,25 +1,44 @@
 package cn.edu.scu.notifyme.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import cn.edu.scu.notifyme.R;
+import cn.edu.scu.notifyme.SettingsActivity;
 
 public class MeFragment extends Fragment {
     private Unbinder unbinder;
+
+    @BindView(R.id.iv_avatar)
+    ImageView ivAvatar;
+    @BindView(R.id.tv_username)
+    TextView tvUsername;
+    @BindView(R.id.slli_settings)
+    LinearLayout slliSettings;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_me, container, false);
         unbinder = ButterKnife.bind(this, view);
+
+        slliSettings.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), SettingsActivity.class);
+            startActivity(intent);
+        });
+
         return view;
     }
 
