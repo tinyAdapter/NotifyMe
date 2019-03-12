c1237aa2fa1b9e3abe3ff86078050868e3d4afb9
tinyAdapter <tinyadapter@outlook.com>
2019-03-12 09:59:36 +0800

    前端验证逻辑

--- app/src/main/java/cn/edu/scu/notifyme/view/SignInFragment.java
+++ app/src/main/java/cn/edu/scu/notifyme/view/SignInFragment.java
@@ -5,17 +5,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.blankj.utilcode.util.RegexUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.google.android.material.textfield.TextInputEditText;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import cn.edu.scu.notifyme.LocaleUtils;
 import cn.edu.scu.notifyme.R;
 import cn.edu.scu.notifyme.SignInSignUpActivity;
 
 public class SignInFragment extends Fragment {
     private Unbinder unbinder;
+
+    @BindView(R.id.et_username)
+    TextInputEditText etUsername;
+    @BindView(R.id.et_password)
+    TextInputEditText etPassword;
 
     @Nullable
     @Override
@@ -35,8 +46,22 @@ public class SignInFragment extends Fragment {
     @OnClick({R.id.btn_sign_in, R.id.btn_sign_up})
     public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.btn_sign_in:
+            case R.id.btn_sign_in: {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                if (username.isEmpty() || password.isEmpty()) {
+                    ToastUtils.showShort(LocaleUtils.getString(
+                            R.string.please_input_username_and_password));
+                    return;
+                }
+                if (!RegexUtils.isUsername(username)) {
+                    ToastUtils.showShort(LocaleUtils.getString(
+                            R.string.please_input_valid_username));
+                    return;
+                }
+                //TODO: 向服务器发送登录请求
                 break;
+            }
             case R.id.btn_sign_up:
                 ((SignInSignUpActivity) getActivity()).setMainFragment(new SignUpFragment());
                 break;
--- app/src/main/java/cn/edu/scu/notifyme/view/SignUpFragment.java
+++ app/src/main/java/cn/edu/scu/notifyme/view/SignUpFragment.java
@@ -5,17 +5,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.blankj.utilcode.util.RegexUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.google.android.material.textfield.TextInputEditText;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
+import cn.edu.scu.notifyme.LocaleUtils;
 import cn.edu.scu.notifyme.R;
 import cn.edu.scu.notifyme.SignInSignUpActivity;
 
 public class SignUpFragment extends Fragment {
     private Unbinder unbinder;
+
+    @BindView(R.id.et_username)
+    TextInputEditText etUsername;
+    @BindView(R.id.et_password)
+    TextInputEditText etPassword;
+    @BindView(R.id.et_repeat_password)
+    TextInputEditText etRepeatPassword;
 
     @Nullable
     @Override
@@ -38,8 +51,29 @@ public class SignUpFragment extends Fragment {
             case R.id.btn_sign_in:
                 ((SignInSignUpActivity) getActivity()).setMainFragment(new SignInFragment());
                 break;
-            case R.id.btn_sign_up:
+            case R.id.btn_sign_up: {
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                String repeatPassword = etRepeatPassword.getText().toString();
+                if (username.isEmpty() || password.isEmpty() || repeatPassword.isEmpty()) {
+                    ToastUtils.showShort(LocaleUtils.getString(
+                            R.string.please_input_username_and_password));
+                    return;
+                }
+                if (!RegexUtils.isUsername(username)) {
+                    ToastUtils.showShort(LocaleUtils.getString(
+                            R.string.please_input_valid_username));
+                    return;
+                }
+                if (!password.equals(repeatPassword)) {
+                    ToastUtils.showShort(LocaleUtils.getString(
+                            R.string.incoherence_passwords
+                    ));
+                    return;
+                }
+                //TODO: 向服务器发送注册请求
                 break;
+            }
         }
     }
 }
